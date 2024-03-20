3f9a833a6c2f3001924b758f3540f95a980e425f
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-03-20 14:37:07 +0100

    [add] titiolo e paragrafo

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# Vue 3 + Vite
+# Vite Hello World
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+Creare nei data un titolo e un paragrafo da stampare nel template.
+Creare  degli stili globali e degli stili specifici del componente App.vue
 
-## Recommended IDE Setup
 
-- [VS Code](https://code.visualstudio.com/) + [Vue - Official](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (previously Volar) and disable Vetur
--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 export default {
   data(){
     return{
+      title:'hello world!',
+      text:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat nam iure tenetur error consectetur accusamus, sit laudantium tempora, reiciendis dicta praesentium cupiditate commodi! Esse eligendi corrupti sunt labore facilis quod. '
 
     }
   }
@@ -11,10 +13,19 @@ export default {
 
 <template>
 
-  <div></div>
+  <h2>{{ title }}</h2>
+  <div>{{ text }}</div>
 
 </template>
 
-<style>
+<style scoped >
+h2{
+  color: cornflowerblue;
+  border: 2px dotted rgb(101, 165, 42);
+  padding: 20px;
 
+}
+div{
+  color: cornflowerblue;
+}
 </style>
